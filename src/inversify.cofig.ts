e215de80cd3ee53734e468044a26d30ec1744891
Prajwal264 <prajwalp264@gmail.com>
2022-08-14 23:52:41 +0530

    binding the instance of board service

--- src/inversify.cofig.ts
+++ src/inversify.cofig.ts
@@ -1,9 +1,11 @@
 import { Container } from 'inversify';
 import TYPES from './types';
 import { AuthService } from './services/auth.service';
+import { BoardService } from './services/board.service';
 
 const container = new Container();
 
 container.bind<AuthService>(TYPES.AuthService).to(AuthService).inSingletonScope();
+container.bind<BoardService>(TYPES.BoardService).to(BoardService).inSingletonScope();
 
 export default container;
